8c98e3dca95046e63a707ce9b0271d897e1c0308
JonasBeer <cc221012@fhstp.ac.at>
2023-11-14 23:47:27 +0100

    added Work in Progress page, installed primevue, primeblocks, primeflex, sass

--- app.vue
+++ app.vue
@@ -1,5 +1,45 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <!--    <NuxtWelcome />-->
+    <!--    <Button label=" test" />-->
+    <div class="surface-section px-4 py-8 md:px-6 lg:px-8">
+      <div class="text-700 text-center">
+        <div class="text-900 font-bold text-5xl mb-3">
+          Hi, I am Jonas Beer 👋
+        </div>
+        <div class="text-700 text-2xl mb-5">
+          And I am an aspiring fullstack developer, <br />
+          currently studying Creative Computing at the UAS St. Pölten.
+        </div>
+        <div class="text-700 text-xl mb-5">
+          This portfolio is a work in progress, so expect changes in the next
+          1-6 business years 🤝
+        </div>
+        <div class="text-700 text-xl mb-5">
+          In the meantime, you can contact me via email:
+        </div>
+        <Button
+          label="Send me a mail"
+          class="font-bold px-5 py-3 p-button-raised p-button-rounded white-space-nowrap"
+          @click="sendMail()"
+        ></Button>
+      </div>
+    </div>
   </div>
 </template>
+<style lang="scss">
+@import "primeflex/primeflex.css";
+Button {
+  margin: 0 auto;
+  display: block;
+  background-color: #19749f;
+}
+</style>
+
+<script setup>
+import { ref } from "vue";
+
+const sendMail = () => {
+  window.open("mailto: contact@jonasbeer.com");
+};
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,22 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  modules: ["nuxt-primevue"],
+  primevue: {
+    options: {
+      ripple: true,
+    },
+    components: {
+      include: ["Button"],
+    },
+  },
+  devtools: {
+    enabled: true,
+
+    timeline: {
+      enabled: true,
+    },
+  },
+  css: ["primevue/resources/themes/lara-light-teal/theme.css"],
+
+  target: "static",
+});
